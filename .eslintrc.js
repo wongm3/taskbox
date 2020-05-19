2ccc4658e8635b1dbde0bcdafaa2c33c5708e752
Matt Wong <matt.wong.rpi@gmail.com>
2020-05-18 23:07:26 -0400

    chore: 🔧 add typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,33 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['airbnb-base'],
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  rules: {
+    'one-var': ['error', 'consecutive'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
---
+++ packages/component-library/src/temp/temp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Temp = () => {
+  return <div>Temp Component to Make lib</div>;
+};
+
+export default Temp;
